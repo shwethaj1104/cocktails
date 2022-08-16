bc032742f52fda19dbe4652094d61aa295c01200
shwethaj1104 <shwethaj1104@gmail.com>
2022-08-16 18:21:38 +0530

    Formatting

--- src/context.jsx
+++ src/context.jsx
@@ -6,22 +6,22 @@ const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
 
 const AppContext = React.createContext()
 
-export const AppProvider = ({children})=>{
-    const [data,setData] = useState(drinks)
-    const [modaldata,setmodalData] = useState({})
-    const [isopenModal,setIsOpenModal] = useState(false)
+export const AppProvider = ({ children }) => {
+    const [data, setData] = useState(drinks)
+    const [modaldata, setmodalData] = useState({})
+    const [isopenModal, setIsOpenModal] = useState(false)
 
-    const openModal = (text)=>{
+    const openModal = (text) => {
         setIsOpenModal(true)
         setmodalData(text)
     }
-    const closeModal = ()=>{
+    const closeModal = () => {
         setIsOpenModal(false)
     }
 
-    return <AppContext.Provider value={{data,isopenModal,openModal,setData,closeModal,modaldata,setmodalData}} >{children}</AppContext.Provider> 
+    return <AppContext.Provider value={{ data, isopenModal, openModal, setData, closeModal, modaldata, setmodalData }} >{children}</AppContext.Provider>
 }
 
-export const useGlobalContext=()=>{
+export const useGlobalContext = () => {
     return useContext(AppContext)
 }
